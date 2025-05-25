67a20cd9cecade01b5d030009d9d27f9af82a481
olumide-as <adigunsamuel84@gmail.com>
2025-05-25 12:12:07 +0100

    push

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+// server.js (or server.ts)
 const { createServer } = require('http');
+const { parse } = require('url');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10) || 3000;
-const app = next({ dev: false });
+const port = parseInt(process.env.PORT || '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
-    handle(req, res);
-  }).listen(port, () => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
