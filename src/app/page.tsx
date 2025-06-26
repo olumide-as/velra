b6a71caa6c61a411cd7bbfad0e38cce45f2bae0b
olumide-as <adigunsamuel84@gmail.com>
2025-06-26 21:59:49 +0100

    push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,13 +169,15 @@ const Home: React.FC = () => {
                   <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
                 </svg>
               </a>
-              <Image
-                src="/assets/designrush.png"
-                alt="DesignRush Logo"
-                width={140}
-                height={30}
-                className="mt-6"
-              />
+              <a href="https://www.designrush.com/" target="_blank" rel="noopener noreferrer">
+                <Image
+                  src="/assets/designrush.png"
+                  alt="DesignRush"
+                  width={140}
+                  height={30}
+                  className="mt-6"
+                />
+              </a>
             </motion.div>
           </div>
         </div>
